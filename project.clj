65b1abdccc466a90b5a67ce937d5752802a67dda
Sun Ning <sunng@about.me>
2014-08-15 17:55:20 +0800

    update dependencies

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject slacker "0.12.1-SNAPSHOT"
+(defproject slacker "0.13.0-SNAPSHOT"
   :description "Transparent, non-invasive RPC by clojure and for clojure"
   :url "http://github.com/sunng87/slacker"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [link "0.6.13"]
+                 [link "0.6.14"]
                  [info.sunng/carbonite "0.2.3"]
                  [cheshire "5.3.1"]
                  [org.clojure/tools.logging "0.3.0"]
@@ -12,7 +12,7 @@
   :profiles {:example {:source-paths ["examples"]
                        :dependencies [[org.clojure/java.jmx "0.2.0"]]}}
   :plugins [[lein-exec "0.3.1"]
-            [codox "0.6.7"]]
+            [codox "0.8.10"]]
   :global-vars {*warn-on-reflection* true}
   :aliases {"run-example-server" ["with-profile" "default,example" "run" "-m" "slacker.example.server"]
             "run-example-client" ["with-profile" "default,example" "run" "-m" "slacker.example.client"]}
